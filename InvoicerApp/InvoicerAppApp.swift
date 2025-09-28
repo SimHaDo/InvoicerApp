af622667a2da3fb3e349c8e8b4951e66d8df06bd
simhado <142127412+SimHaDo@users.noreply.github.com>
2025-09-27 22:32:48 -0400

    Initial Commit

---
+++ InvoicerApp/InvoicerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoicerAppApp.swift
+//  InvoicerApp
+//
+//  Created by Danyil Skrypnichenko on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct InvoicerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
